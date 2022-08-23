53f5d24d7dde1246dac84fd14b46f543781901c7
Alexandr <yaromahob@gmail.com>
2022-08-23 22:40:15 +0300

    feat: counter logic, add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  const [total, setTotal] = React.useState(0);
+
+  const onIncrement = () => {
+    setTotal(total + 1);
+    console.log(total);
+  };
+  const onDecrement = () => {
+    setTotal(total - 1);
+    console.log(total);
+  };
+
+  return (
+    <div className="counter-wrapper">
+      <button onClick={onIncrement}>+</button>
+      <p>{total}</p>
+      <button onClick={onDecrement}>-</button>
+    </div>
+  );
 };
 
 export default App;
